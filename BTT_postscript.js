85b548912c6ecf1841d97aeba8877e9c947a5072
daniel shinbaum <dashinbaum@gmail.com>
2022-07-06 18:06:24 +0200

    Fix verbatim paths in BTT_postscript.js

--- BTT_postscript.js
+++ BTT_postscript.js
@@ -1,5 +1,5 @@
 if (Translator.BetterTeX && item.attachments) {
   item.attachments.forEach((att, i) => {
-    entry.add({ name: `bdsk-file-${i + 1}`, value: ((att.saveFile ? att.defaultPath : '') || att.localPath) })
+    entry.add({ name: `bdsk-file-${i + 1}`, value: (att.saveFile ? att.defaultPath : '') || att.localPath, enc: 'verbatim' })
   })
 }
