d918de0e32fe1da7245f191c0a06de245a115e2b
daniel shinbaum <dashinbaum@gmail.com>
2022-07-15 19:46:46 +0200

    Use "~" in File path

--- BTT_postscript.js
+++ BTT_postscript.js
@@ -3,3 +3,15 @@ if (Translator.BetterTeX && item.attachments) {
     entry.add({ name: `bdsk-file-${i + 1}`, value: (att.saveFile ? att.defaultPath : '') || att.localPath, enc: 'verbatim' })
   })
 }
+
+// https://retorque.re/zotero-better-bibtex/exporting/scripting/#use--in-file-paths-to-avoid-the-bib-file-being-different-on-different-computers
+// https://forums.zotero.org/discussion/comment/399187#Comment_399187
+if (Translator.BetterTeX && !Translator.options.exportFileData && item.attachments && item.attachments.length) {
+  for (const att of item.attachments) {
+    if (att.localPath) {
+      att.localPath = att.localPath.replace(RegExp("^\/.*?\/.*?\/"), "~/")
+    }
+  }
+  entry.add({ name: 'file', value: item.attachments, enc: 'attachments' })
+  return { cache: false }
+}
