38a38b93dd965820dcdf3145f76b335383838638
daniel shinbaum <dashinbaum@gmail.com>
2022-07-21 03:25:35 +0200

    Fix (?) postscript. maybe.

--- BTT_postscript.js
+++ BTT_postscript.js
@@ -1,17 +1,14 @@
-if (Translator.BetterTeX && item.attachments) {
-  item.attachments.forEach((att, i) => {
-    entry.add({ name: `bdsk-file-${i + 1}`, value: (att.saveFile ? att.defaultPath : '') || att.localPath, enc: 'verbatim' })
-  })
-}
-
-// https://retorque.re/zotero-better-bibtex/exporting/scripting/#use--in-file-paths-to-avoid-the-bib-file-being-different-on-different-computers
-// https://forums.zotero.org/discussion/comment/399187#Comment_399187
-if (Translator.BetterTeX && !Translator.options.exportFileData && item.attachments && item.attachments.length) {
-  for (const att of item.attachments) {
-    if (att.localPath) {
-      att.localPath = att.localPath.replace(RegExp("^\/.*?\/.*?\/"), "~/")
+if (Translator.BetterTeX) {
+  if (!Translator.options.exportFileData && item.attachments && item.attachments.length) {
+    item.attachments.forEach((att, i) => {
+      entry.add({ name: `bdsk-file-${i + 1}`, value: (att.saveFile ? att.defaultPath : '') || att.localPath, enc: 'verbatim' })
+    })
+    for (const att of item.attachments) {
+      if (att.localPath) {
+        att.localPath = att.localPath.replace(RegExp("^\/.*?\/.*?\/"), "~/")
+      }
     }
+    entry.add({ name: 'file', value: item.attachments, enc: 'attachments' })
+    return { cache: false }
   }
-  entry.add({ name: 'file', value: item.attachments, enc: 'attachments' })
-  return { cache: false }
 }
