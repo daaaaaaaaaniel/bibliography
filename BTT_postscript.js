25f363b779a295ae90a3fe7194745aeeaa2420bb
daniel shinbaum <dashinbaum@gmail.com>
2022-07-21 22:30:05 +0200

    WIP update postscript to split subtitle from title

--- BTT_postscript.js
+++ BTT_postscript.js
@@ -17,10 +17,38 @@ if (Translator.BetterTeX) {
   // No need to test for the value, BBT does nothing if the value is empty
   entry.add({ name: 'chapter', value: extra.kv.session});
   entry.add({ name: 'journalAbbreviation', value: extra.kv.journalAbbreviation});
+  // BROKEN - Split long Book Title into Book Title, Book Subtitle
+  // if (reference.has.publicationTitle) {
+  if (reference.has.booktitle) {
+    const booktitles = reference.booktitle.value.match(/(.+?)([?:;])(.*)/)
+    if (booktitles) {
+      let booktitle = booktitles[1] + (booktitles[2] === '?' ? '?' : '')
+      reference.add({ name: 'booktitle', value: booktitle })
+      reference.add({ name: 'booksubtitle', value: booktitles[3].trim() })
+    }
+  }
+  // Split long Title into Title, Subtitle
+  // https://github.com/retorquere/zotero-better-bibtex/issues/1759#issuecomment-798949707
+  if (reference.has.title) {
+    const titles = reference.has.title.value.match(/(.+?)([?:;])(.*)/)
+    if (titles) {
+      let title = titles[1] + (titles[2] === '?' ? '?' : '')
+      reference.add({ name: 'title', value: title })
+      reference.add({ name: 'subtitle', value: titles[3].trim() })
+    }
+  }
   return { cache }
 }
+
 
   // if (item.extra) {
   //   // reference.add({ name: 'note', value: item.extra.split('\n').filter(line => !line.startsWith('chapter-number:')).join('\n') })
   //   reference.add({ name: 'chapter', value: item.extra.replace(/^.*?\s*?.*?(chapter-number:\s+)(\S+)[\s]*.*/mg, '$2') });
   // }
+
+  // BROKEN - Split long Title into Title, Subtitle
+  // if (item.title) {
+  //   this.add({ name: 'subtitle', value: item.title.replace(/^.* ?: (.*)/g, '$1') });
+  //   this.add({ name: 'title', value: item.title.replace(/^(\S*) ?: .+/g, '$1') });
+  // }
+  //
